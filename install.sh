8134590e779ae985470a66098d987b6dd552e71f
Samuel Heinz <54530346+samuelh0@users.noreply.github.com>
2022-07-01 12:07:16 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo pacman -Syu # To get the latest package lists
 sudo pacman -S git yay packagekit-qt5 flatpak fwupd # Install Preq
-yay -S google-chrome spotify tidal-hifi steam discord polymc discover onlyoffice-bin ntfs-3g protonvpn mate-system-monitor gnome-system-monitor ocs-url pling-store teamviewer # Install most packages
+yay -S google-chrome spotify tidal-hifi steam discord polymc discover onlyoffice-bin ntfs-3g protonvpn mate-system-monitor gnome-system-monitor ocs-url pling-store teamviewer discover-overlay # Install most packages
 teamviewer # to make sure teamviewerd is avaliable 
 systemctl enable --now teamviewerd # enabling teamviewerd
 cd ~/Documents
