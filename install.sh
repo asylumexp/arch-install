360e2e14fb94a450fe301eea0c5e6a1395dc0526
Samuel Heinz <54530346+samuelh0@users.noreply.github.com>
2022-07-01 11:22:26 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo pacman -S git yay packagekit-qt5 flatpak fwupd # Install Preq
 yay -S google-chrome spotify tidal-hifi steam discord polymc discover onlyoffice-bin ntfs-3g protonvpn mate-system-monitor gnome-system-monitor ocs-url pling-store teamviewer # Install most packages
 teamviewer # to make sure teamviewerd is avaliable 
 systemctl enable --now teamviewerd # enabling teamviewerd
-cd Documents
+cd ~/Documents
 git clone https://github.com/Frogging-Family/nvidia-all.git
 cd nvidia-all
 makepkg -si # nvidia drivers
